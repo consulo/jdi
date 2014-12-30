dfbdab6aa4a52c90b6e0e4c6fbc2afd15968a7d9
VISTALL <vistall.valeriy@gmail.com>
2014-12-30 13:55:07 +0300

    format

--- src/consulo/internal/com/sun/tools/jdi/JDWPException.java
+++ src/consulo/internal/com/sun/tools/jdi/JDWPException.java
@@ -24,45 +24,57 @@
  */
 
 package consulo.internal.com.sun.tools.jdi;
-import consulo.internal.com.sun.jdi.*;
 
-class JDWPException extends Exception {
-    private static final long serialVersionUID = -6321344442751299874L;
-    short errorCode;
+import consulo.internal.com.sun.jdi.ClassNotPreparedException;
+import consulo.internal.com.sun.jdi.InconsistentDebugInfoException;
+import consulo.internal.com.sun.jdi.InternalException;
+import consulo.internal.com.sun.jdi.InvalidStackFrameException;
+import consulo.internal.com.sun.jdi.ObjectCollectedException;
+import consulo.internal.com.sun.jdi.VMDisconnectedException;
+import consulo.internal.com.sun.jdi.VMOutOfMemoryException;
 
-    JDWPException(short errorCode) {
-        super();
-        this.errorCode = errorCode;
-    }
+class JDWPException extends Exception
+{
+	private static final long serialVersionUID = -6321344442751299874L;
+	short errorCode;
 
-    short errorCode() {
-        return errorCode;
-    }
+	JDWPException(short errorCode)
+	{
+		super();
+		this.errorCode = errorCode;
+	}
 
-    RuntimeException toJDIException() {
-        switch (errorCode) {
-            case JDWP.Error.INVALID_OBJECT:
-                return new ObjectCollectedException();
-            case JDWP.Error.VM_DEAD:
-                return new VMDisconnectedException();
-            case JDWP.Error.OUT_OF_MEMORY:
-                return new VMOutOfMemoryException();
-            case JDWP.Error.CLASS_NOT_PREPARED:
-                return new ClassNotPreparedException();
-            case JDWP.Error.INVALID_FRAMEID:
-            case JDWP.Error.NOT_CURRENT_FRAME:
-                return new InvalidStackFrameException();
-            case JDWP.Error.NOT_IMPLEMENTED:
-                return new UnsupportedOperationException();
-            case JDWP.Error.INVALID_INDEX:
-            case JDWP.Error.INVALID_LENGTH:
-                return new IndexOutOfBoundsException();
-            case JDWP.Error.TYPE_MISMATCH:
-                return new InconsistentDebugInfoException();
-            case JDWP.Error.INVALID_THREAD:
-                return new IllegalThreadStateException();
-            default:
-                return new InternalException("Unexpected JDWP Error: " + errorCode, errorCode);
-        }
-    }
+	short errorCode()
+	{
+		return errorCode;
+	}
+
+	RuntimeException toJDIException()
+	{
+		switch(errorCode)
+		{
+			case JDWP.Error.INVALID_OBJECT:
+				return new ObjectCollectedException();
+			case JDWP.Error.VM_DEAD:
+				return new VMDisconnectedException();
+			case JDWP.Error.OUT_OF_MEMORY:
+				return new VMOutOfMemoryException();
+			case JDWP.Error.CLASS_NOT_PREPARED:
+				return new ClassNotPreparedException();
+			case JDWP.Error.INVALID_FRAMEID:
+			case JDWP.Error.NOT_CURRENT_FRAME:
+				return new InvalidStackFrameException();
+			case JDWP.Error.NOT_IMPLEMENTED:
+				return new UnsupportedOperationException();
+			case JDWP.Error.INVALID_INDEX:
+			case JDWP.Error.INVALID_LENGTH:
+				return new IndexOutOfBoundsException();
+			case JDWP.Error.TYPE_MISMATCH:
+				return new InconsistentDebugInfoException();
+			case JDWP.Error.INVALID_THREAD:
+				return new IllegalThreadStateException();
+			default:
+				return new InternalException("Unexpected JDWP Error: " + errorCode, errorCode);
+		}
+	}
 }
